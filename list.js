21bda2783c19e43e39f253eb09de02b1228a5f9a
olecsiy19 <olecsiy19@gmail.com>
2016-08-16 10:07:11 +0300

    sun

--- list.js
+++ list.js
@@ -11,25 +11,16 @@ ContactList.controller('ListCtrl', function ($scope,$interval) {
     };
 
     $scope.contacts = [];
-    $scope.workList = [];
-    $scope.homeList = [];
     $scope.hours = [];
-    $scope.coords = {};
 
-    if(localStorage.workList){
-        angular.forEach(JSON.parse(localStorage.workList), function(list) {
-            pushContact($scope.workList, list);
-        });
-    }
-
-    if(localStorage.homeList){
-        angular.forEach(JSON.parse(localStorage.homeList), function(list) {
-            pushContact($scope.homeList, list);
+    if(localStorage.contacts){
+        angular.forEach(JSON.parse(localStorage.contacts), function(list) {
+            pushContact($scope.contacts, list);
         });
     }
 
     if(localStorage.userInformations) {
-        $scope.show = true;
+        $scope.showButtom = true;
         renameInformationAboutUser(JSON.parse(localStorage.userInformations));
     }
     else {
@@ -37,150 +28,145 @@ ContactList.controller('ListCtrl', function ($scope,$interval) {
     }
 
     $scope.getUserInformations = function() {
-
         renameInformationAboutUser($scope.userLocation);
         $scope.user.$setPristine();
         $scope.userCity = '';
         localStorage.userInformations = JSON.stringify($scope.userInformations);
-        $scope.show = true;     
+        $scope.showButtom = true;     
     };
 
-    $scope.addContact = function () {
+    $scope.addContact = function() {
         $scope.locationCity.name = $scope.contactName;
-
-        if($scope.group) {
-            pushContact($scope.homeList, $scope.locationCity);
-            localStorage.homeList = JSON.stringify($scope.homeList);
-        }
-        else {
-            pushContact($scope.workList, $scope.locationCity);
-            localStorage.workList = JSON.stringify($scope.workList);
-        }
+        
+        pushContact($scope.contacts, $scope.locationCity);
+        localStorage.contacts = JSON.stringify($scope.contacts);
 
         $scope.addNewContact.$setPristine();
         $scope.cityName = '';
         $scope.contactName = '';
         $scope.locationCity = '';
     };
-    $scope.rectangleHeight = function () {
+
+    $scope.rectangleHeight = function() {
         return document.getElementsByClassName("table")[0].offsetHeight + 'px';
     };
-    $scope.rectangle = function () {
-        var a = new Date().getUTCHours();
-        var b = document.getElementsByClassName("time");
-        if(document.getElementsByClassName("time")[a]) {
-            $scope.rectangleWidth = document.getElementsByClassName("time")[a].offsetWidth + 'px';
+
+    $scope.rectangle = function() {
+        var hourNow = new Date().getUTCHours();
+        var timeBlock = document.getElementsByClassName("time");
+
+        if(timeBlock[hourNow]) {
+            $scope.rectangleWidth = timeBlock[hourNow].offsetWidth + 'px';
         }
-        return angular.element(b[a]).prop('offsetLeft') + 'px';
+
+        return angular.element(timeBlock[hourNow]).prop('offsetLeft') + 'px';
     };
 
-    $scope.onMouseOver = function ($event) {
-        $scope.onMouseMoveResult = angular.element($event.target).prop('offsetLeft') + 'px';
+    $scope.onMouseOver = function($event) {
+        $scope.rectangleLeft = angular.element($event.target).prop('offsetLeft') + 'px';
         $scope.rectangleWidth = angular.element($event.target).prop('offsetWidth') + 'px'
     };
 
-    $scope.onMouseLeave = function () {
-
-        $scope.onMouseMoveResult = undefined;
+    $scope.onMouseLeave = function() {
+        $scope.rectangleLeft = undefined;
     };
 
-    $scope.workTime = function (hour) {
-        return (hour>8 && hour<19);
-    };
-
-    $scope.lightDay = function (hour, contact) {
-        if(contact.sunrise < contact.sunset)
-            return (hour > contact.sunrise && hour < contact.sunset);
-        else if (contact.sunrise > contact.sunset)
-            return !(hour > contact.sunset && hour < contact.sunrise);
-    };
-
-    $scope.firstLastHour = function(hour, periodOfTheDay) {
-        return (hour == periodOfTheDay);
-    };
-
-    $scope.loadList = function() {
-        if($scope.group)
-            $scope.contacts = $scope.homeList;
-        else
-            $scope.contacts = $scope.workList;
-    }
-
-    $scope.timer  = function () {
-
-        $scope.clock = new Date();
-
-        var updateClock = function () {
-            $scope.clock = new Date();
-        };
-
-        setInterval(function () {
-            $scope.$apply(updateClock);
-        }, 1000);
-
-        updateClock();
-    };
-    $scope.timer();
 
     $scope.addHours = function(UTC){
-
         timeZone = parseInt(UTC);
         $scope.hours[0] = (timeZone <= 0)? timeZone + 24 : timeZone;
 
-        for(var i = 1; i < 24; ++i) {
+        for (var i = 1; i < 24; ++i) {
             $scope.hours[i] = ($scope.hours[i-1] == 24)? 1 : $scope.hours[i-1] + 1;
         }
     };
 
     $scope.removeContact = function(index) {
-        if($scope.group) {
-            $scope.homeList.splice(index, 1);
-            localStorage.homeList = JSON.stringify($scope.homeList);
-        }
-        else {
-            $scope.workList.splice(index, 1);
-            localStorage.workList = JSON.stringify($scope.workList);
-        }
+        $scope.contacts.splice(index, 1);
+        localStorage.contacts = JSON.stringify($scope.contacts);
     };
 
     $scope.removeUserInformation = function() {
         localStorage.removeItem("userInformations");
         getUserInformationsByIp();
-        $scope.show = false;
+        $scope.showButtom = false;
     };
 
-    function renameInformationAboutUser (information) {
+    var timer  = function() {
+        $scope.clock = new Date();
 
+        var updateClock = function() {
+            $scope.clock = new Date();
+        };
+
+        setInterval( function() {
+            $scope.$apply(updateClock);
+        }, 1000);
+
+        updateClock();
+    }();
+
+    function renameInformationAboutUser(information) {
         $scope.userInformations.location = information.location;
         $scope.userInformations.timeZoneName = information.timeZoneName;
         $scope.userInformations.UTC = moment.tz(information.timeZoneName).format('Z');
         $scope.userInformations.latitude = information.latitude;
         $scope.userInformations.longitude = information.longitude;
-        $scope.userInformations.sunrise = getSunrise(information.latitude, information.longitude, information.timeZoneName);
-        $scope.userInformations.sunset = getSunset(information.latitude, information.longitude, information.timeZoneName);
+        $scope.userInformations.sunrise = getSunriseOrSanset(information.latitude, information.longitude,
+         information.timeZoneName, 'sunrise');
+        $scope.userInformations.sunset = getSunriseOrSanset(information.latitude, information.longitude,
+         information.timeZoneName, 'sunset');
     }
 
     function getUserInformationsByIp() {
-
-
         var xhr = new XMLHttpRequest();
         xhr.open('get', 'https://freegeoip.net/json/', true);
         xhr.responseType = 'json';
 
         xhr.onload = function() {
-            
             var cord = xhr.response;
             $scope.userInformations.location = cord.city;
             $scope.userInformations.timeZoneName = cord.time_zone;
             $scope.userInformations.UTC = moment.tz(cord.time_zone).format('Z');
             $scope.userInformations.latitude = cord.latitude;
             $scope.userInformations.longitude = cord.longitude;
-            $scope.userInformations.sunrise = getSunrise($scope.userInformations.latitude, $scope.userInformations.longitude, $scope.userInformations.timeZoneName);
-            $scope.userInformations.sunset = getSunset($scope.userInformations.latitude, $scope.userInformations.longitude, $scope.userInformations.timeZoneName);
+            $scope.userInformations.sunrise = getSunriseOrSanset($scope.userInformations.latitude,
+             $scope.userInformations.longitude, $scope.userInformations.timeZoneName, 'sunrise');
+            $scope.userInformations.sunset = getSunriseOrSanset($scope.userInformations.latitude,
+             $scope.userInformations.longitude, $scope.userInformations.timeZoneName, 'sunset');
         };
         xhr.send();
     }   
     
+    function getSunriseOrSanset(latitude, longitude, timeZoneName, dayPeriod) {
+
+        var times = SunCalc.getTimes(new Date(), latitude, longitude);
+        var sun = times[dayPeriod].getUTCHours() + parseInt(moment.tz(timeZoneName).format('Z'));
+
+        if (sun < 1) {
+            sun += 24;
+        } 
+        else if (sun > 24) {
+            sun -= 24;
+        }
+
+        return sun;
+    }
+
+    function pushContact(list , contact) {
+
+        list.push({
+            name: contact.name,
+            location: contact.location,
+            timeZoneName: contact.timeZoneName,
+            UTC: moment.tz(contact.timeZoneName).format('Z'),
+            latitude: contact.latitude,
+            longitude: contact.longitude,
+            sunrise: getSunriseOrSanset(contact.latitude, contact.longitude, contact.timeZoneName, 'sunrise'),
+            sunset: getSunriseOrSanset(contact.latitude, contact.longitude, contact.timeZoneName, 'sunset')
+        });
+    }
+
 }); 
 
 ContactList.directive('googleplace', function() {
@@ -197,7 +183,8 @@ ContactList.directive('googleplace', function() {
                     return new Promise(function(resolve, reject) {
 
                         var xhr = new XMLHttpRequest();
-                        xhr.open('get', 'https://maps.googleapis.com/maps/api/timezone/json?location='+latitude+','+longitude+'&timestamp=1331766000&sensor=false', true);
+                        xhr.open('get', 'https://maps.googleapis.com/maps/api/timezone/json?location=' + latitude 
+                         + ',' + longitude + '&timestamp=1331766000&sensor=false', true);
                         xhr.responseType = 'json';
 
                         xhr.onload = function() {
@@ -221,7 +208,7 @@ ContactList.directive('googleplace', function() {
                 getTimeZone(city.latitude, city.longitude).then(function(timeZoneId) {
 
                     city.timeZoneName = timeZoneId;
-                    if(attrs.name === 'userLocation') {
+                    if (attrs.name === 'userLocation') {
                         scope.getUserInformations();
                     }
                 });
@@ -233,46 +220,88 @@ ContactList.directive('googleplace', function() {
     };
 });
 
+ContactList.directive('sunAndWorkDay', function () {
+    return {
 
-function getSunrise(latitude, longitude, timeZoneName) {
+        link: function (scope, element, attrs) {
+            scope.elementList = scope[attrs["sunAndWorkDay"]];
+            var hour = scope[attrs["ngBind"]];
 
-    var times = SunCalc.getTimes(new Date(), latitude, longitude);
-    sunrise = times.sunrise.getUTCHours() + parseInt(moment.tz(timeZoneName).format('Z'));
+            scope.$watch(function() {
+                    return angular.toJson([scope.group, scope.elementList]);
+                }, function() {
+                if (scope.group) {
+                    element.removeClass("work_hour not_work_hour day night sunrise sunset");
+                    element.removeClass("day_sun night_sun sunrise_sun sunset_sun");
 
-    if(sunrise < 1) {
-        sunrise += 24;
-    }
-    else if(sunrise > 24) {
-        sunrise -= 24;
-    }
+                    if(scope.elementList.sunrise < scope.elementList.sunset) {
+                        if (hour > scope.elementList.sunrise && hour < scope.elementList.sunset) {
+                            element.addClass("day_sun");
+                        } 
+                        else {
+                            element.addClass("night_sun");
+                        }
+                    } 
+                    else if (scope.elementList.sunrise > scope.elementList.sunset) {
+                        if (!(hour > scope.elementList.sunset && hour < scope.elementList.sunrise)) {
+                            element.removeClass('day');
+                            element.addClass("day_sun");
+                        } 
+                        else {
+                            element.addClass("night_sun");
+                        }
+                    } 
+                    else {
+                        element.addClass("night_sun");
+                    }
 
-    return sunrise;
-}
+                    if(hour == scope.elementList.sunrise) {
+                        element.addClass("sunrise_sun");
+                    }
 
-function getSunset(latitude, longitude, timeZoneName) {
+                    if(hour == scope.elementList.sunset) {
+                        element.addClass("sunset_sun");
+                    }
+                } 
+                else {
+                    element.removeClass("work_hour not_work_hour day night sunrise sunset");
+                    element.removeClass("day_sun night_sun sunrise_sun sunset_sun");
+                    if (hour > 8 && hour < 19) {
+                        element.addClass("work_hour");
+                    } 
+                    else {
+                        element.addClass("not_work_hour");
+                    }
 
-    var times = SunCalc.getTimes(new Date(), latitude, longitude);
-    sunset = times.sunset.getUTCHours() + parseInt(moment.tz(timeZoneName).format('Z'));
+                    if(scope.elementList.sunrise < scope.elementList.sunset) {
+                        if (hour > scope.elementList.sunrise && hour < scope.elementList.sunset) {
+                            element.addClass("day");
+                        } 
+                        else {
+                            element.addClass("night");
+                        }
+                    } 
+                    else if (scope.elementList.sunrise > scope.elementList.sunset) {
+                        if (!(hour > scope.elementList.sunset && hour < scope.elementList.sunrise)) {
+                            element.addClass("day");
+                        } 
+                        else {
+                            element.addClass("night");
+                        }
+                    } 
+                    else {
+                        element.addClass("night");
+                    }
 
-    if(sunset < 1) {
-        sunset += 24;
-    }
-    else if(sunset > 24) {
-        sunset -= 24;
-    }
-    return sunset;
-}
+                    if(hour == scope.elementList.sunrise) {
+                        element.addClass("sunrise");
+                    }
 
-function pushContact(list , contact) {
-
-    list.push({
-        name: contact.name,
-        location: contact.location,
-        timeZoneName: contact.timeZoneName,
-        UTC: moment.tz(contact.timeZoneName).format('Z'),
-        latitude: contact.latitude,
-        longitude: contact.longitude,
-        sunrise: getSunrise(contact.latitude, contact.longitude, contact.timeZoneName),
-        sunset: getSunset(contact.latitude, contact.longitude, contact.timeZoneName)
-    });
-}
+                    if(hour == scope.elementList.sunset) {
+                        element.addClass("sunset");
+                    }
+                }                
+            });
+        }
+    };
+});
